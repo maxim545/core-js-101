42ef9fcde3e5bd539c735b905a9377c1b270365b
maxim545 <maxximc95@gmail.com>
2022-07-21 20:02:32 +0300

    feat: add solving for task cssSelectorBuilder

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -1,3 +1,4 @@
+
 /* ************************************************************************************************
  *                                                                                                *
  * Please read the following tutorial before implementing tasks:                                   *
@@ -116,35 +117,170 @@ function fromJSON(proto, json) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+
+  str: '',
+  elArr: [],
+  idArr: [],
+  pseudoArr: [],
+  arr: [],
+  elementsArr: [],
+  arrCombinator: [],
+  countArr: [],
+  count: 0,
+  errObj: {
+    el: 'Element, id and pseudo-element should not occur more then one time inside the selector',
+    sel: 'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    if (!this.countArr.includes(0)) { this.countArr.push(0); }
+    this.countArr.forEach(() => {
+      if (this.countArr[0] > this.countArr[1] && this.countArr.length <= 2) {
+        throw new Error(this.errObj.sel);
+      }
+    });
+    if (this.elArr.includes('element') && !this.arrCombinator.length) {
+      throw new Error(this.errObj.el);
+    } else {
+      this.elArr.push('element');
+    }
+    this.elementsArr.push(value);
+    if (this.count >= 1) {
+      this.str = `${this.str}&${value}`;
+    } else {
+      this.str += value;
+    }
+    this.idArr = [];
+    this.pseudoArr = [];
+    this.count += 1;
+    this.arr.push(value);
+    return this;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    if (!this.countArr.includes(1)) { this.countArr.push(1); }
+    this.countArr.forEach(() => {
+      if (this.countArr[0] > this.countArr[1] && this.countArr.length <= 2) {
+        throw new Error(this.errObj.sel);
+      }
+    });
+    if (this.idArr.includes('id') && !this.arrCombinator.length) {
+      throw new Error(this.errObj.el);
+    } else {
+      this.idArr.push('id');
+    }
+    this.elArr = [];
+    this.pseudoArr = [];
+    this.str += `#${value}`;
+    this.arr.push(`#${value}`);
+    return this;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    if (!this.countArr.includes(2)) { this.countArr.push(2); }
+    this.countArr.forEach(() => {
+      if (this.countArr[0] > this.countArr[1] && this.countArr.length <= 2) {
+        throw new Error(this.errObj.sel);
+      }
+    });
+    this.elArr = [];
+    this.idArr = [];
+    this.pseudoArr = [];
+    this.str += `.${value}`;
+    this.arr.push(`.${value}`);
+    return this;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    if (!this.countArr.includes(3)) { this.countArr.push(3); }
+    this.countArr.forEach(() => {
+      if (this.countArr[0] > this.countArr[1] && this.countArr.length <= 2) {
+        throw new Error(this.errObj.sel);
+      }
+    });
+    this.elArr = [];
+    this.idArr = [];
+    this.pseudoArr = [];
+    this.str += `[${value}]`;
+    this.arr.push(`[${value}]`);
+    return this;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    if (!this.countArr.includes(4)) { this.countArr.push(4); }
+    this.countArr.forEach(() => {
+      if (this.countArr[0] > this.countArr[1] && this.countArr.length <= 2) {
+        throw new Error(this.errObj.sel);
+      }
+    });
+    this.elArr = [];
+    this.idArr = [];
+    this.pseudoArr = [];
+    this.str += `:${value}`;
+    this.arr.push(`:${value}`);
+    return this;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    if (!this.countArr.includes(5)) { this.countArr.push(5); }
+    this.countArr.forEach(() => {
+      if (this.countArr[0] > this.countArr[1] && this.countArr.length <= 2) {
+        throw new Error(this.errObj.sel);
+      }
+    });
+    if (this.pseudoArr.includes('pseudoElement') && !this.arrCombinator.length) {
+      throw new Error(this.errObj.el);
+    } else {
+      this.pseudoArr.push('pseudoElement');
+    }
+    this.elArr = [];
+    this.idArr = [];
+    this.str += `::${value}`;
+    this.arr.push(`::${value}`);
+    return this;
   },
+
+  // eslint-disable-next-line no-unused-vars
+  combine(selector1, combinator, selector2) {
+    this.arrCombinator.push(combinator);
+    return this;
+  },
+
+  clear() {
+    this.elArr = [];
+    const copiedArr = [...this.arr];
+    const copiedArrCombinator = this.arrCombinator.reverse();
+    const copiedEleemts = this.elementsArr;
+    let str = '';
+    if (copiedArrCombinator.length) {
+      copiedEleemts.forEach((el, i) => {
+        if (i > 0 && copiedEleemts.length > 1) {
+          const index = copiedArr.indexOf(el);
+          copiedArr.splice(index, 1, ` ${copiedArrCombinator.shift()} ${el}`);
+        } else if (i === 0 && copiedEleemts.length === 1) {
+          const index = copiedArr.indexOf(el);
+          copiedArr.splice(index, 1, ` ${copiedArrCombinator.shift()} ${el}`);
+        }
+      });
+    }
+    copiedArr.forEach((el) => {
+      str += el;
+    });
+    this.str = '';
+    this.arr = [];
+    this.elementsArr = [];
+    this.arrCombinator = [];
+    this.count = 0;
+    this.countArr = [];
+    return str;
+  },
+
 };
 
+// eslint-disable-next-line no-extend-native
+Object.prototype.stringify = function stringify() {
+  return this.clear();
+};
 
 module.exports = {
   Rectangle,
